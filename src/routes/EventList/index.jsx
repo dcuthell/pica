2a236219fd076d146009d7c4ca04edc8a168f1fe
David Cuthell <dcc1447@gmail.com>
2019-08-14 12:16:42 -0700

    update eventlist

--- src/routes/EventList/index.jsx
+++ src/routes/EventList/index.jsx
@@ -10,11 +10,11 @@ export default function EventList (props) {
     <Container style={{padding: '0', margin: '0px', maxWidth: '100%'}}>
       <Row>
         <Col xl='12' style={{height: '10vh'}}>
-          <h1>Tickets</h1>
+          <h1>Event List</h1>
         </Col>
         <Col xl='12'>
           <div style={{height: '90vh', overflowY: 'scroll'}}>
-            <iframe src='https://system.spektrix.com/pica/website/EventCalendar.aspx' title='SpektrixIFrame' name='SpektrixIFrame' id='SpektrixIFrame' style={{frameBorder: '0', height: '100%', width: '80%', border: 'none'}} onLoad={scrollup()} />
+            <iframe src='https://system.spektrix.com/pica/website/EventList.aspx?MonthSelect=20199&SortBy=Date' title='SpektrixIFrame' name='SpektrixIFrame' id='SpektrixIFrame' style={{frameBorder: '0', height: '100%', width: '80%', border: 'none'}} onLoad={scrollup()} />
           </div>
         </Col>
       </Row>
