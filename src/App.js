7e2d7188ab78dbb996b271b0c42cfa7df8aa18fa
David Cuthell <dcc1447@gmail.com>
2019-08-14 13:21:06 -0700

    add code for eventlist

--- src/App.js
+++ src/App.js
@@ -6,16 +6,14 @@ import './App.css'
 import PicaNavBar from './components/PicaNavBar'
 import PicaBody from './components/PicaBody'
 
-import TBA from './routes/TBA'
-import TBAEvent from './routes/TBAEvent'
+import EventList from './routes/EventList'
 
 function App() {
   return (
     <div className='App'>
       <PicaNavBar/>
       <PicaBody>
-        <Route exact path='/' component={TBA} />
-        <Route path='/:id' component={TBAEvent} />
+        <Route exact path='/' component={EventList} />
       </PicaBody>
     </div>
   );
