7a193b8fbf8f0395bd18ba7aa0f3855ce5b0a522
David Cuthell <dcc1447@gmail.com>
2019-09-08 14:48:45 -0700

    ppop

--- src/routes/Blog/index.jsx
+++ src/routes/Blog/index.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import { Container, Col, Row } from 'reactstrap'
 
+function importSpektrix() {
+  const tag = document.createElement('script')
+  const link = docum
+  tag.async = false
+  tag.src = 'https://system.spektrix.com/pica/api/v3/webcomponents/js'
+
+
+}
+
 export default function Blog (props) {
   return (
     <Container style={{padding: '0', margin: '0px', maxWidth: '100%'}}>
@@ -11,6 +20,7 @@ export default function Blog (props) {
         <Col xl='12'>
           <h1>STUFF</h1>
         </Col>
+        <spektrix-basket-item-count></spektrix-basket-item-count>
       </Row>
     </Container>
   )
