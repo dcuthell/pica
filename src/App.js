f1cf853eacfc6527d7aa6cb510f777caaf06e2ca
David Cuthell <dcc1447@gmail.com>
2019-10-03 22:43:56 -0700

    update support page with more routes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import PicaNavBar from './components/PicaNavBar'
 import PicaBody from './components/PicaBody'
 
 import Support from './routes/Support'
+import Donations from './routes/Donations'
+import Members from './routes/Members'
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
       <PicaNavBar/>
       <PicaBody>
         <Route exact path='/' component={Support} />
+        <Route exact path='/donations' component={Donations} />
+        <Route exact path='/members' component={Members} />
       </PicaBody>
     </div>
   );
